a34bccb8674f6a92ed208825afa82c88ec81a148
Valentin <kibirevvalik202@gmail.com>
2021-03-15 18:18:22 +0300

    [add] Project

---
+++ HotBall/Assets/_scripts/DeformPlane.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeformPlane : MonoBehaviour
+{
+    [SerializeField]
+    private MeshFilter _meshFilter;
+    private Mesh _mesh;
+
+    private Vector3[] _verts;
+
+    [SerializeField]
+    private float _radius, _power;
+    void Start()
+    {
+        _mesh = _meshFilter.mesh;
+        _verts = _mesh.vertices;
+    }
+
+    public void DeformMesh(Vector3 positionToDeform )
+    {
+        positionToDeform = transform.InverseTransformPoint(positionToDeform);
+
+        for (int i = 0; i < _verts.Length; i++)
+        {
+            float dist = (_verts[i] - positionToDeform).sqrMagnitude;
+            if (dist<_radius)
+            {
+                _verts[i] -= Vector3.back * _power;
+            }
+            
+        }
+        _mesh.vertices = _verts;
+    }
+}
---
+++ HotBall/Assets/_scripts/RayCam.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCam : MonoBehaviour
+{
+    private Ray _ray;
+    private RaycastHit _raycastHit;
+
+    private Camera _cam;
+    private void Start()
+    {
+        _cam = Camera.main;
+
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            DeformMesh();
+        }
+    }
+    private void DeformMesh()
+    {
+        _ray = _cam.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(_ray,out _raycastHit))
+        {
+            DeformPlane deformPlane = _raycastHit.transform.GetComponent<DeformPlane>();
+            deformPlane.DeformMesh(_raycastHit.point);
+        }
+    }
+}
